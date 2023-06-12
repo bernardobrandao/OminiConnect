20cf575b319b22d700e16faae6282eacb99e54d5
bernardo brandao <bernardonbrandao@gmail.com>
2023-06-12 19:43:42 -0300

    Criadas as Classes de Models.

---
+++ OminiConnect/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace OminiConnect.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ OminiConnect/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace OminiConnect.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PreferredChannel { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
